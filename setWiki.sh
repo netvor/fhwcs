02152465f445d538c114abe31d796ae4121ec038
Floris <floris@flyinghonzas.cz>
2010-06-08 16:05:44 +0200

    First enhancements for UVN

--- setWiki.sh
+++ setWiki.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-DWPAGE=/srv/www/beta.flyinghonzas.cz/htdocs/bin/dwpage.php
+DWPAGE=/srv/www/uvn.flyinghonzas.cz/htdocs/bin/dwpage.php
 
 $DWPAGE -m "Updated by doWiki.sh" commit 'wiki/out.txt' '2010 fifa world cup sweeps'
