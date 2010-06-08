e2fe60f6fbee19a808781587215811a348a1b2f7
Floris <floris@flyinghonzas.cz>
2010-06-08 16:42:39 +0200

    New layout of match pages --> new parser

--- parseMatch.py
+++ parseMatch.py
@@ -6,16 +6,17 @@ from whohas import whohas
 
 tree = etree.parse(getInFile(''),etree.HTMLParser())
 
-div = tree.xpath('//div[@id="fwcMatchHeaderBgPre"]')[0]
+div = tree.xpath('//div[@id="fwcMatchHeader"]')[0]
 ms = MatchStat()
-ms.number = int(div.xpath('./div[@class="top"]/span[@class="L"]')[0].text[6:])
-ms.group = div.xpath('./div[@class="top"]/span[@class="bold L"]/a')[0].text[6:].upper()
-ms.when = div.xpath('./div[@class="top"]/span[@class="R"]')[0].text
-ms.homeCode = div.xpath('./div[@class="middle"]/div[@class="hFlag flagImg "]/a/img')[0].get('src')[-7:-4].upper()
-ms.homeName = div.xpath('./div[@class="middle"]/div[@class="teamh"]/a')[0].text
+ms.number = int(div.xpath('./div[@class="footer"]/div[@class="info"]/span[@class="matchInfo L"]')[0].text[6:])
+ms.group = div.xpath('./div[@class="footer"]/div[@class="info"]/span[@class="L"]/a')[0].text[6:].upper()
+ms.when = div.xpath('./div[@class="footer"]/div[@class="info"]/span[@class="L"]')[1].text[3:] + \
+          " " + div.xpath('./div[@class="match"]/div[@class="time"]')[0].text
+ms.homeCode = div.xpath('./div[@class="match"]/div[@class="teamH"]/div[@class="flag"]/a/img')[0].get('src')[-7:-4].upper()
+ms.homeName = div.xpath('./div[@class="match"]/div[@class="teamH"]/div[@class="name"]/a')[0].text
 ms.homeWiki = u':%s: **%s** (%s)' % (ms.homeCode, whohas[ms.homeCode], ms.homeName)
-ms.awayCode = div.xpath('./div[@class="middle"]/div[@class="aFlag flagImg"]/a/img')[0].get('src')[-7:-4].upper()
-ms.awayName = div.xpath('./div[@class="middle"]/div[@class="teama"]/a')[0].text
+ms.awayCode = div.xpath('./div[@class="match"]/div[@class="teamA"]/div[@class="flag"]/a/img')[0].get('src')[-7:-4].upper()
+ms.awayName = div.xpath('./div[@class="match"]/div[@class="teamA"]/div[@class="name"]/a')[0].text
 ms.awayWiki = u':%s: **%s** (%s)' % (ms.awayCode, whohas[ms.awayCode], ms.awayName)
 ms.hasResults = False
 
