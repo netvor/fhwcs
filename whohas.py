b91f8ca6909e5a055b340f5bbd826fec174e08f2
Floris van der Meijs <incoming@harpavieja.cz>
2010-06-07 22:52:09 -0700

    Added payment reminder to whohas

--- whohas.py
+++ whohas.py
@@ -2,33 +2,33 @@
 
 whohas = { 'ALG' : u'Martine' ,
            'ARG' : u'George' ,
-           'AUS' : u'Sonia' ,
-           'BRA' : u'Vendulka' ,
-           'CHI' : u'Ailee' ,
+           'AUS' : u'Sonia' , # needs to pay
+           'BRA' : u'Vendulka' , # needs to pay
+           'CHI' : u'Ailee' , # needs to pay
            'CIV' : u'Quido' ,
            'CMR' : u'Lisa' ,
            'DEN' : u'Bas' ,
            'ENG' : u'George' ,
            'ESP' : u'Katka' ,
            'FRA' : u'Bas' ,
-           'GER' : u'Harmen' ,
+           'GER' : u'Harmen' , # needs to pay
            'GHA' : u'AJ' ,
            'GRE' : u'Paul' ,
            'HON' : u'Sue' ,
-           'ITA' : u'Rudy' ,
+           'ITA' : u'Rudy' , # needs to pay
            'JPN' : u'Marty' ,
-           'KOR' : u'Rudy' ,
+           'KOR' : u'Rudy' , # needs to pay
            'MEX' : u'Sue' ,
-           'NED' : u'Floris' ,
+           'NED' : u'Floris' , # needs to pay
            'NGA' : u'Pippa' ,
            'NZL' : u'George' ,
            'PAR' : u'Martine' ,
            'POR' : u'Jens' ,
-           'PRK' : u'Sonia' ,
-           'RSA' : u'Scott' ,
-           'SRB' : u'Olivia' ,
+           'PRK' : u'Sonia' , # needs to pay
+           'RSA' : u'Scott' , # needs to pay
+           'SRB' : u'Olivia' , # needs to pay
            'SUI' : u'Paul' ,
            'SVK' : u'Viktor' ,
            'SVN' : u'Jens' ,
            'URU' : u'AJ' ,
-           'USA' : u'Harmen' }
+           'USA' : u'Harmen' } # needs to pay
