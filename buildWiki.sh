6dece0e211dbf8995fcb93c7bdd169ef350d4816
Floris <floris@flyinghonzas.cz>
2010-06-09 15:09:37 +0200

    Split doWiki.sh to buildWiki.sh and setWiki.sh

--- buildWiki.sh
+++ buildWiki.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DWPAGE=/srv/www/beta.flyinghonzas.cz/htdocs/bin/dwpage.php
-
 ./fetchData.sh
 
 python wikiGSS.py       > 'wiki/gss.txt'
@@ -13,5 +11,3 @@ cat 'wiki/gss.txt'       >> 'wiki/out.txt'
 cat 'wiki/lsigs.txt'     >> 'wiki/out.txt'
 cat 'wiki/calendar.txt'  >> 'wiki/out.txt'
 cat 'wiki/footer.txt'    >> 'wiki/out.txt'
-
-$DWPAGE -m "Updated by doWiki.sh" commit 'wiki/out.txt' '2010 fifa world cup sweeps'
---
+++ setWiki.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DWPAGE=/srv/www/beta.flyinghonzas.cz/htdocs/bin/dwpage.php
+
+$DWPAGE -m "Updated by doWiki.sh" commit 'wiki/out.txt' '2010 fifa world cup sweeps'
