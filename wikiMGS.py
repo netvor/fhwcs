c5ef64431047b936bded30ac4d5e2ab6ad7bfdea
Floris <floris@flyinghonzas.cz>
2010-07-08 12:21:20 +0200

    Changed wikiMGS.py to include goals against

--- wikiMGS.py
+++ wikiMGS.py
@@ -5,19 +5,22 @@ from operator import itemgetter
 from optfunc import optfunc
 
 def wikiMGSheader():
-  return '^ %-50s ^  Goals scored  ^' % ('Team')
+  return '^ %-50s ^  Goals scored  ^  Goals against  ^' % ('Team')
 
 def wikiMGSrow(team, goals):
-  return '| %-50s | %14d |' % (team,goals)
+  gf='**%d**' % (goals[0])
+  return '| %-50s | %14s | %15d |' % (team,gf,-goals[1])
 
 def wikiMGS(num=10):
   mgs=dict()
   for ms in unmarshalMatches('parsed/match%02d.pkl'):
     if ms.hasResults:
       for team in [ms.homeWiki(), ms.awayWiki()]: 
-        if team not in mgs: mgs[team] = 0
-      mgs[ms.homeWiki()] += len(ms.homeGoals)
-      mgs[ms.awayWiki()] += len(ms.awayGoals)
+        if team not in mgs: mgs[team] = [0,0]
+      mgs[ms.homeWiki()][0] += len(ms.homeGoals)
+      mgs[ms.awayWiki()][0] += len(ms.awayGoals)
+      mgs[ms.awayWiki()][1] -= len(ms.homeGoals)
+      mgs[ms.homeWiki()][1] -= len(ms.awayGoals)
 
   print '\n\n==== Most goals scored ====\n'.encode('utf-8')
   print wikiMGSheader().encode('utf-8')
