e695a1564e7a4f644b9ef24502e7d12b3b73a208
Floris <floris@flyinghonzas.cz>
2010-07-12 15:55:20 +0200

    Added prefix to wikiscripts

--- wikiFG.py
+++ wikiFG.py
@@ -1,25 +1,29 @@
 ﻿#!/usr/bin/python
 
-from globals import MatchStat, unmarshalMatches
+from globals import MatchStat, unmarshalMatches, counter
 from operator import itemgetter
 from optfunc import optfunc
 
-def wikiFGheader():
-  return '^ %-50s ^  Minute  ^' % ('Team')
+def wikiFGheader(prefix):
+  if prefix: return '^  #  ^ %-50s ^  Minute  ^' % ('Team')
+  else: return '^ %-50s ^  Minute  ^' % ('Team')
 
-def wikiFGrow(team, minute):
-  return "| %-50s | %7d' |" % (team,minute)
+def wikiFGrow(team, minute,prefix):
+  if prefix:
+    return "| %3d | %-50s | %7d' |" % (counter.next(),team,minute)
+  else:
+    return "| %-50s | %7d' |" % (team,minute)
 
-def wikiFG(num=10):
+def wikiFG(num=10,prefix=False):
   goals=list()
   for ms in unmarshalMatches('parsed/match%02d.pkl'):
     if ms.hasResults:
       goals += ms.goals()
 
   print '\n\n==== Fastest goal ====\n'.encode('utf-8')
-  print wikiFGheader().encode('utf-8')
+  print wikiFGheader(prefix).encode('utf-8')
 
   for (minute,team) in sorted(goals, key=itemgetter(0))[:num]:
-    print wikiFGrow(team,minute).encode('utf-8')
+    print wikiFGrow(team,minute,prefix).encode('utf-8')
 
 optfunc.main(wikiFG)
--- wikiMGPG.py
+++ wikiMGPG.py
@@ -1,17 +1,19 @@
 ﻿#!/usr/bin/python
 
-from globals import MatchStat, unmarshalMatches
+from globals import MatchStat, unmarshalMatches, counter
 from operator import attrgetter
 from optfunc import optfunc
 
-def wikiMGPGheader():
-  return '^  No.  ^       When       ^  %-88s  ^  Goals scored  ^' % ('Match')
+def wikiMGPGheader(prefix):
+  if prefix: return '^  #  ^  No.  ^       When       ^  %-88s  ^  Goals scored  ^' % ('Match')
+  else: return '^  No.  ^       When       ^  %-88s  ^  Goals scored  ^' % ('Match')
 
-def wikiMGPGrow(ms):
+def wikiMGPGrow(ms,prefix):
   _goals = '**%d** (%d:%d)' % (ms.totalGoals, len(ms.homeGoals), len(ms.awayGoals))
-  return '| %5d | %16s | %-90s | %-14s |' % (ms.number, ms.when, ms.matchTitle(), _goals)
+  if prefix: return '| %3d | %5d | %16s | %-90s | %-14s |' % (counter.next(),ms.number, ms.when, ms.matchTitle(), _goals)
+  else: return '| %5d | %16s | %-90s | %-14s |' % (ms.number, ms.when, ms.matchTitle(), _goals)
 
-def wikiMGPG(num=10):
+def wikiMGPG(num=10,prefix=False):
   matches=list()
   for ms in unmarshalMatches('parsed/match%02d.pkl'):
     if ms.hasResults:
@@ -19,9 +21,9 @@ def wikiMGPG(num=10):
       matches.append(ms)
 
   print '\n\n==== Most goals per game ====\n'.encode('utf-8')
-  print wikiMGPGheader().encode('utf-8')
+  print wikiMGPGheader(prefix).encode('utf-8')
 
   for ms in list(sorted( sorted(matches,key=lambda x:x.when_tm()) ,key=attrgetter('totalGoals'), reverse=True))[:num]:
-    print wikiMGPGrow(ms).encode('utf-8')
+    print wikiMGPGrow(ms,prefix).encode('utf-8')
 
 optfunc.main(wikiMGPG)
